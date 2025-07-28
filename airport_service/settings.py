1a3954d3331a816f1d7227665d730c7cfda63d4d
Tetiana Sobko <sobkotiana@gmail.com>
2025-07-28 21:01:21 +0300

    Add user

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'flights',
+    'bookings',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'user.User'
