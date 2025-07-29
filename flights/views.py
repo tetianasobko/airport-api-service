0f6cab2ecb5c582ea38f46e009be562693f81c62
Tetiana Sobko <sobkotiana@gmail.com>
2025-07-30 01:06:32 +0300

    Add serializers, views, and urls for Airport and Routes models

--- flights/views.py
+++ flights/views.py
@@ -3,12 +3,19 @@ from rest_framework import viewsets
 from flights.models import (
     City,
     Country,
-)
+    Airport,
+    Route,)
 
 from flights.serializers import (
     CountrySerializer,
     CitySerializer,
     CityListSerializer,
+    AirportSerializer,
+    AirportListSerializer,
+    RouteSerializer,
+    RouteListSerializer,
+    AirportDetailSerializer,
+    RouteDetailSerializer,
 )
 
 
@@ -25,3 +32,27 @@ class CityViewSet(viewsets.ModelViewSet):
         if self.action in ("list", "retrieve"):
             return CityListSerializer
         return CitySerializer
+
+
+class AirportViewSet(viewsets.ModelViewSet):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return AirportListSerializer
+        elif self.action == "retrieve":
+            return AirportDetailSerializer
+        return AirportSerializer
+
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return RouteListSerializer
+        elif self.action == "retrieve":
+            return RouteDetailSerializer
+        return RouteSerializer
