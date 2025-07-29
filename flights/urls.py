cd3a3b9802fd249b7ced560bb0da3f2a9c561cd7
Tetiana Sobko <sobkotiana@gmail.com>
2025-07-29 23:29:57 +0300

    Add serializers, views, and urls for Country and City models

---
+++ flights/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from flights.views import (
+    CountryViewSet,
+    CityViewSet,
+)
+
+router = routers.DefaultRouter()
+router.register("countries", CountryViewSet)
+router.register("cities", CityViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
+
+app_name = "flights"
