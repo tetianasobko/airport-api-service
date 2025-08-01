06d5e98249932730ff54035d0a39db22bcb70593
Tetiana Sobko <sobkotiana@gmail.com>
2025-08-01 14:41:39 +0300

    Create models for bookings app

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+from flights.models import Flight
+
+
+class SeatClass(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Seat Classes"
+        ordering = ["name"]
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey("user.User", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (
+            f"Order by {self.user.username} "
+            f"on {self.created_at.strftime('%Y-%m-%d %H:%M:%S')}"
+        )
+
+    class Meta:
+        ordering = ["-created_at"]
+
+
+class Ticket(models.Model):
+    row = models.PositiveIntegerField()
+    seat = models.PositiveIntegerField()
+    flight = models.ForeignKey(Flight, on_delete=models.CASCADE)
+    seat_class = models.ForeignKey(SeatClass, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (
+            f"Ticket for {self.flight.airline.name} Flight "
+            f"from {self.flight.route.source.name} "
+            f"to {self.flight.route.destination.name} - "
+            f"Row {self.row}, Seat {self.seat} ({self.seat_class.name})"
+        )
+
+    class Meta:
+        ordering = ["flight__departure_time", "row", "seat"]
+        unique_together = ("flight", "row", "seat", "seat_class")
