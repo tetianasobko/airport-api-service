b1671a602586c90a8286c2407e4ea29ac59daf82
Tetiana Sobko <sobkotiana@gmail.com>
2025-08-12 00:26:14 +0300

    Register bookings app models

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
-# Register your models here.
+from bookings.models import Order, Ticket, SeatClass
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "created_at")
+    search_fields = ("user__username",)
+    list_filter = ("created_at",)
+    ordering = ("-created_at",)
+    date_hierarchy = "created_at"
+    readonly_fields = ("created_at",)
+
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = (
+        "id", "row", "seat", "flight", "seat_class", "price", "order"
+    )
+    search_fields = (
+        "flight__airline__name",
+        "flight__route__source__name",
+        "flight__route__destination__name",
+        "order__user__username"
+    )
+    list_filter = ("seat_class", "flight__departure_time")
+    ordering = ("flight__departure_time", "row", "seat")
+    readonly_fields = ("price",)
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.select_related("flight", "seat_class", "order")
+
+
+admin.site.register(SeatClass)
