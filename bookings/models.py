286d6b40917ad31fface995fca60dc9ed5a6c2b3
Tetiana Sobko <sobkotiana@gmail.com>
2025-08-19 00:21:25 +0300

    Implement validation for Ticket model

--- bookings/models.py
+++ bookings/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 from flights.models import Flight, SeatClass, Compartment
 
@@ -39,3 +40,58 @@ class Ticket(models.Model):
     class Meta:
         ordering = ["flight__departure_time", "row", "seat"]
         unique_together = ("flight", "row", "seat", "seat_class")
+
+    @staticmethod
+    def validate_seat(flight, row, seat, seat_class, error_to_raise):
+        try:
+            compartment = flight.airplane.compartments.get(
+                seat_class=seat_class
+            )
+        except Compartment.DoesNotExist:
+            raise error_to_raise(
+                "This seat class doesn't exist on this airplane"
+            )
+
+        if row < compartment.start_row or row > compartment.end_row:
+            raise error_to_raise(
+                f"Row {row} is not in the valid range for {seat_class.name}"
+            )
+
+        if seat not in compartment.seats_in_row:
+            raise error_to_raise(
+                f"Seat {seat} is not valid for this compartment"
+            )
+
+    @staticmethod
+    def validate_flight(flight: Flight, error_to_raise):
+        if flight.status in ("cancelled", "departed", "arrived"):
+            raise error_to_raise(
+                "You cannot buy tickets for a flight that is "
+                f"{flight.status}."
+            )
+
+        if flight.departure_time <= timezone.now():
+            raise error_to_raise(
+                "You cannot buy tickets for a flight "
+                "that has already departed."
+            )
+
+    def clean(self):
+        Ticket.validate_seat(
+            self.flight,
+            self.row,
+            self.seat,
+            self.seat_class,
+            ValueError
+        )
+        Ticket.validate_flight(self.flight, ValueError)
+
+    def save(
+            self,
+            force_insert=False,
+            force_update=False,
+            using=None,
+            update_fields=None
+    ):
+        self.full_clean()
+        super().save(force_insert, force_update, using, update_fields)
