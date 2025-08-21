50727a2804ca8b8874fb751345433670580630f0
Tetiana Sobko <sobkotiana@gmail.com>
2025-08-21 20:11:53 +0300

    Add IsAdminOrOwner permission

--- airport_service/permissions.py
+++ airport_service/permissions.py
@@ -7,3 +7,11 @@ class IsAdminOrReadOnly(BasePermission):
             return True
 
         return request.user and request.user.is_staff
+
+
+class IsAdminOrOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.user and request.user.is_staff:
+            return True
+
+        return obj.user == request.user
