232c8e57d62a9bc36ea6942cb02e20b11011c00f
Tetiana Sobko <sobkotiana@gmail.com>
2025-08-28 08:32:44 +0300

    Configure secret key from environment variable

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9_*)s@q3!s6t^8(srttv8wtaq%$b)n5c7=-3s6qzgblv+$-uoj'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
